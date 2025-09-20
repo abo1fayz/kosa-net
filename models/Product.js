f8274fbe071b5338f38285a6bd1afba1eeb27c21
abo1fayz <145799866+abo1fayz@users.noreply.github.com>
2025-09-20 16:57:33 +0300

    Create Product.js

---
+++ models/Product.js
@@ -0,0 +1,35 @@
+// models/Product.js
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'اسم المنتج إجباري.'],
+    trim: true,
+    unique: true
+  },
+  description: {
+    type: String,
+    required: [true, 'وصف المنتج إجباري.']
+  },
+  price: {
+    type: Number,
+    required: [true, 'سعر المنتج إجباري.'],
+    min: [0, 'السعر لا يمكن أن يكون أقل من 0.']
+  },
+  category: {
+    type: String,
+    required: [true, 'التصنيف إجباري.'],
+    enum: ['إلكترونيات', 'ملابس', 'أثاث', 'كتب', 'أخرى']
+  },
+  imageURL: {
+    type: String,
+    required: [true, 'رابط الصورة إجباري.']
+  }
+}, {
+  timestamps: true
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
